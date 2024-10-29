eae7a8ba51713aaace7242e3e9ece7394762860f
agrawalprince115@gmail.com <agrawalprince115@gmail.com>
2024-10-29 22:52:07 +0530

    nothing change

--- C++/file_handling.cpp
+++ C++/file_handling.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
      ofstream file1;
      file1.open("D:\\Prince\\C++\\aaa1.txt");
-     file1<<"this is new file cefwe fwe oweo woehf wohfhfwqfqwf qdqw ";
-    //  cout<<"data written in the file";
+     file1<<"hi i am prince kumar agrawal";
+     cout<<"data written in the file";
      file1.close();
 
-    ifstream file;
-    string str;
-    file.open("D:\\Prince\\C++\\aaa1.txt");
-    // file>>str;
-    while(getline(file,str)){
-        cout<<str;
-    }
+    // ifstream file;
+    // string str;
+    // file.open("D:\\Prince\\C++\\aaa1.txt");
+    // // file>>str;
+    // while(getline(file,str)){
+    //     cout<<str;
+    // }
     
-    file.close();
+    // file.close();
     return 0;
 }
