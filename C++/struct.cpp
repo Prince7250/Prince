5ea122e070de4e50fa341006402549b0b197cb27
agrawalprince115@gmail.com <agrawalprince115@gmail.com>
2024-11-20 20:28:52 +0530

    add space

--- C++/struct.cpp
+++ C++/struct.cpp
@@ -4,13 +4,13 @@ using namespace std;
 // What is struct ?
 // struct is keyword, it is use to group several related variable in once.
 
-struct area {
+typedef struct area {
     int l,b;
-};
+}area;
 
 int main(){
-struct area a1;
-struct area a2;
+area a1;
+area a2;
 a1.l = 12;
 a1.b = 43;
 cout<<" area of first rectangle : "<<a1.l*a1.b;
@@ -18,7 +18,6 @@ cout<<" area of first rectangle : "<<a1.l*a1.b;
 a2.l = 43;
 a2.b = 5;
 cout<<"\narea of secont rectrangle : "<<a2.l*a2.b;
-
 
 return 0;
 }
