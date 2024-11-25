a6c7921f61bc87b6a9a4a21ad9b1b41678f589e1
agrawalprince115@gmail.com <agrawalprince115@gmail.com>
2024-11-26 00:52:20 +0530

    add acess private member function

---
+++ C++/accessPrivateMem.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class stud
+{
+private:
+    void privateMem()
+    {
+        cout << "This is Private Member function ";
+    }
+
+    public :
+    void callPrivateMem(){
+        privateMem();
+    }
+    // friend void privateMem(stud &st); // Private function access outside the class 
+
+};
+// void privateMem(stud &st)
+// {
+//     st.privateMem();
+// }
+
+int main()
+{
+    // stud s12;
+    // privateMem(s12);
+     stud s1;
+     s1.callPrivateMem();
+    return 0;
+}
