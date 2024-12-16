0240bb88a4896a15ea04369e0ed68db1962f444d
Prince7250 <agrawalprince115@gmail.com>
2024-12-17 02:07:21 +0530

    function overriding using virtual

--- C++/virtual_function.cpp
+++ C++/virtual_function.cpp
@@ -1,21 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class stud{
+class stud
+{
     string name = "Prince";
     int roll = 341243243;
-    public :
-   virtual void detail(){
-    cout<<"Name : "<<name<<endl;
+
+public:
+    virtual void detail()
+    {
+        cout << "Name : " << name << endl;
     }
 };
 
-class stud2 : public stud {
+class stud2 : public stud
+{
     string name = "Rohit";
     int roll = 23523543;
-    public :
-    void detail(){
-        cout<<"Name : "<<name<<endl;
-        cout<<"Roll : "<<roll<<endl;
+
+public:
+    void detail()
+    {
+        cout << "Name : " << name << endl;
+        cout << "Roll : " << roll << endl;
     }
 };
 
@@ -31,6 +37,6 @@ int main()
     stud2 s2;
     s1 = &s2;
     s1->detail();
-    
-return 0;
+
+    return 0;
 }
