921d3a77cf886b40e38f33a97dc51a45b1b8c50d
Prince7250 <agrawalprince115@gmail.com>
2024-12-17 15:02:28 +0530

    nothing

--- C++/copy_constructor.cpp
+++ C++/copy_constructor.cpp
@@ -1,35 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class add{
-    int a ,b;
-    public :
-    add(){ a = 2; b = 5;}
-    add(int x,int y){
+class add
+{
+    int a, b;
+
+public:
+    add()
+    {
+        a = 2;
+        b = 5;
+    }
+    add(int x, int y)
+    {
         a = x;
         b = y;
     }
 
-    //  add(add &obj){
-    //     a = obj.a;
-    //     b = obj.b;
-    //  }
+    add(add &obj)
+    {
+        cout << "\nCopy constructor called" << endl;
+        a = obj.a;
+        b = obj.b;
+    }
 
-    void dis(){
-        cout<<a<<" "<<b<<endl;
+    void dis()
+    {
+        cout << a << " " << b << endl;
     }
 };
 int main()
 {
- add a1, a2(23,35);
- a1.dis();
- a2.dis();
- 
- add a3(a2);
- a3.dis();
+    add a1, a2(23, 35);
+    a1.dis();
+    a2.dis();
 
- add a4(a3);
- a4.dis();
+    add a3(a1);
+    a3.dis();
 
 
-return 0;
+    return 0;
 }
---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+(add &obj)
+    // {
+    //     cout << "\nCopy constructor called" << endl;
+    //     a = obj.a;
+    //     b = obj.b;
