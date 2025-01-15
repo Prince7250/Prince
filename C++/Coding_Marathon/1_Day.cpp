c2826f90bd0d38c6d8f34e4eb2777ecb3b139e62
Prince7250 <agrawalprince115@gmail.com>
2025-01-15 19:41:39 +0530

    update

--- C++/Coding_Marathon/1_Day.cpp
+++ C++/Coding_Marathon/1_Day.cpp
@@ -1,4 +1,5 @@
 // //1) Write a program to show how to read and write integer values from user.
+
 // #include<iostream>
 // using namespace std;
 // int main(){
@@ -9,7 +10,10 @@
 //  return 0;
 //  }
 
+
+
  //2) Input two numbers x and y from keyboard you need to print the sum of two numbers with 2 decimal digit.
+
 #include<iostream>
 #include<iomanip> 
 using namespace std;
