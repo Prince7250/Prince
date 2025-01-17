2ee8aca34b219d0230b9891ab36052c17431d1f0
Prince7250 <agrawalprince115@gmail.com>
2025-01-18 00:19:28 +0530

    4th day complete

--- C++/Coding_Marathon/4th_Day.cpp
+++ C++/Coding_Marathon/4th_Day.cpp
@@ -1,15 +1,43 @@
 //1) Write a program to input any number from user and check whether the given number is perfect number or not.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i,num,sum=0;
+//     cin>>num;
+//     for(i = 1; i < num; i++){
+//         if(num % i == 0){
+//             sum += i;
+//         }
+//     }
+//    cout<<((sum == num) ? "yes" : "no");        
+//    return 0;
+// }
+
+
+//2) Write a program to print the n lines of the below pyramid
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// Important Note: Don't print spaces at the end of the last row.
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int i,num,sum=0;
-    cin>>num;
-    for(i = 1; i < num; i++){
-        if(num % i == 0){
-            sum += i;
+    int i , j , n , num = 1;
+    cin>>n;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
+            cout<<num;
+            if (j < i) {
+                cout << " ";
+            }
+            num++;
         }
+        cout<<endl;
     }
-   cout<<((sum == num) ? "yes" : "no");        
+    
    return 0;
 }
