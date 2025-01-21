f1f4293decc0da674e05b2fc10522f2889df4c6c
Prince7250 <agrawalprince115@gmail.com>
2025-01-21 18:09:38 +0530

    java update

---
+++ JAVA/app.java
@@ -0,0 +1,8 @@
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 23, b = 34;
+        System.out.println(a - b);
+    }
+}
+    
