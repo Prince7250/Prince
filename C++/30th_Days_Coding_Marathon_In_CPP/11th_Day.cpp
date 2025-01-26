c17cd5d39bdde0df99d39e1dea28a23c127f9d74
Prince7250 <agrawalprince115@gmail.com>
2025-01-26 18:07:16 +0530

    updatd

--- C++/30th_Days_Coding_Marathon_In_CPP/11th_Day.cpp
+++ C++/30th_Days_Coding_Marathon_In_CPP/11th_Day.cpp
@@ -1,5 +1,4 @@
 //1)  Write a recursive function sumOfDigits that accepts an integer as a parameter and returns the sum of its digits. For example, calling sumOfDigits(1729) should return 1 + 7 + 2 + 9, which is 19. If the number is negative, return the negation of the value. For example, calling sumOfDigits(-1729) should return -19.
-
 // Constraints: Do not declare any global variables. Do not use any loops; you must use recursion. Also do not solve this problem using a string. You can declare as many primitive variables like ints as you like. You are allowed to define other "helper" functions if you like; they are subject to these same constraints.
 
 // #include <iostream>
@@ -23,6 +22,8 @@
     
 //     return 0;
 // }
+
+
 
 // 2) Prime factors using recursion 
 // Write a program that accepts an integer n (where n >=2) and print all the prime factors of n using recursion.
