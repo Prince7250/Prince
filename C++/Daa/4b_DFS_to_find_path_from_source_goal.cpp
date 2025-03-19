df188546544f85f84a9eeaca42454241cfdc2b51
Prince7250 <agrawalprince115@gmail.com>
2025-03-19 23:13:45 +0530

    nothing

--- C++/Daa/4b_DFS_to_find_path_from_source_goal.cpp
+++ C++/Daa/4b_DFS_to_find_path_from_source_goal.cpp
@@ -6,40 +6,49 @@ using namespace std;
 vector<int> adj[10];
 bool visited[10];
 
-void dfs(int s) {
+void dfs(int s)
+{
     visited[s] = true;
-    for (int i = 0; i < adj[s].size(); ++i) {
-        if (!visited[adj[s][i]]) {
+    for (int i = 0; i < adj[s].size(); ++i)
+    {
+        if (!visited[adj[s][i]])
+        {
             dfs(adj[s][i]);
         }
     }
 }
 
-void initialize() {
-    for (int i = 0; i < 10; ++i) {
+void initialize()
+{
+    for (int i = 0; i < 10; ++i)
+    {
         visited[i] = false;
     }
 }
 
-int main() {
+int main()
+{
     int nodes, edges, x, y, connectedComponents = 0;
     cin >> nodes >> edges;
-    
-    for (int i = 0; i < edges; ++i) {
+
+    for (int i = 0; i < edges; ++i)
+    {
         cin >> x >> y;
         adj[x].push_back(y);
         adj[y].push_back(x);
     }
-    
+
     initialize();
-    
-    for (int i = 1; i <= nodes; ++i) {
-        if (!visited[i]) {
+
+    for (int i = 1; i <= nodes; ++i)
+    {
+        if (!visited[i])
+        {
             dfs(i);
             connectedComponents++;
         }
     }
-    
+
     cout << "Number of connected components: " << connectedComponents << endl;
     return 0;
 }
