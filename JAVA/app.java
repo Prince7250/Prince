c999d716bc5a05c0e84b1d30102461ad81ba447d
Prince7250 <agrawalprince115@gmail.com>
2026-06-12 10:19:37 +0530

    update java code

--- JAVA/app.java
+++ JAVA/app.java
@@ -1,8 +1,36 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("This is program 1");
-        System.out.println(4 + 5);
-        System.out.println(4 * 5);
+    //     System.out.println("Hello World");
+    //     System.out.print("This is program 1");
+        // String nam = "hell1o";
+        // String nam1 = "hello";
+        // System.out.println(nam = nam1);
+
+        // System.out.println(4 + 5);
+        // System.out.println(4 * 5);
+     int x = 5;
+     int y = 10;
+     int z = 2;
+    //    x += 5 * 2;
+    // boolean result = (++x * y / z % 3 + 2 - 1 > 10) && ((x << y << 1) == 12) || ((y & z) == 2 ? true : false);
+    //   System.out.println(result); 
+
+    //   System.out.println(x++ + ++x * x++);   
+    //   System.out.println(--x + ++x * --x);
+    //   System.out.println(20 - 10 - 5);
+    //   System.out.println(x);
+    // System.out.println(- 8 >> 1);
+    // System.out.println(8 >> 1 << 2);
+
+      int count = 0;
+      for (int i = 0; i < 9; i++) {
+        // System.out.println(i);
+          for (int j = 0; j < i; j++) {
+             System.out.print("*");
+          }
+            System.out.println();
+
+      }
+
     }
 }
---
+++ JAVA/loop.java
@@ -0,0 +1,19 @@
+class loop {
+    public static void main(String[] args) {
+        // int i = 12345, j = 0;
+        // int k = 0;
+        // while (i > 0){
+        // k = k * 10+(i/10);
+        // i /= 10;
+        // // j++;
+        // }
+        // // if(i == 123) continue;
+        // System.out.println(k);
+        int i = 1;
+        do {
+           
+        } while (i < 0);
+        System.out.println(i);
+
+    }
+}
